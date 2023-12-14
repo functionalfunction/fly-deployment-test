0367683584bdf2c5a8674b1045656d10e0390aee
functionalfunction <giraysoyturk@gmail.com>
2023-12-14 23:17:57 +0300

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, Fly.io!\n");
+  res.end("Hello, Fly.io and Dede! \n");
 });
 
 const PORT = process.env.PORT || 3000;
